b724f4b4bc67115ffe14c5c651155ff2f2dbea3a
Valeri Karpov <val@karpov.io>
2024-11-28 11:48:23 -0500

    export async button correctly

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
 
+exports.asyncButton = require('./components/async-button/async-button');
 exports.dropdown = require('./components/dropdown/dropdown');
 exports.modal = require('./components/modal/modal');
